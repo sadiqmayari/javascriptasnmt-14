e3520bbdf8eb7d11f6195a64fd7d3d07b74bdfbe
Muhammad Sadiq <sadiqmayari@gmail.com>
2023-12-19 20:24:32 +0500

    title case

--- app.js
+++ app.js
@@ -1,133 +1,137 @@
-
-document.write("<br><br><br>")
+document.write("<br><br><br>");
 
 //date function
-function getDate(){
-    currentDate = new Date()
-    return currentDate
+function getDate() {
+  currentDate = new Date();
+  return currentDate;
 }
-document.write(getDate())
+document.write(getDate());
 
-document.write("<br><br><br>")
-
+document.write("<br><br><br>");
 
 //full name
-function fullName(){
-   var fisrtName= prompt("What is your First Name?")
-   var lastName =  prompt("What is Your Last Name?")
-    return fisrtName +" "+lastName
+function fullName() {
+  var fisrtName = prompt("What is your First Name?");
+  var lastName = prompt("What is Your Last Name?");
+  return fisrtName + " " + lastName;
 }
-alert(fullName())
-
+alert(fullName());
 
 //usr input sum
-function sum(){
-    var a = parseFloat(prompt("Enter any number:"))
-    var b = parseFloat(prompt("Enter second number: "))
+function sum() {
+  var a = parseFloat(prompt("Enter any number:"));
+  var b = parseFloat(prompt("Enter second number: "));
 
-    return a+b
-    
+  return a + b;
 }
 
-document.write("sum of input value ==> "+sum())
+document.write("sum of input value ==> " + sum());
 
-document.write("<br><br><br>")
-
-
+document.write("<br><br><br>");
 
 //calculator by  entry
-function calc(){
-    var num1 = parseFloat(prompt("Enter any number:"))
-    var num2 = parseFloat(prompt("Enter second number: "))
-    var operator = prompt("Put Sign of Add(+) or Sub(-) or Multipy(*) or Divide(/)")
+function calc() {
+  var num1 = parseFloat(prompt("Enter any number:"));
+  var num2 = parseFloat(prompt("Enter second number: "));
+  var operator = prompt(
+    "Put Sign of Add(+) or Sub(-) or Multipy(*) or Divide(/)"
+  );
 
-    
-    if(operator === "+"){
-        return num1+num2
-    }
-    else if(operator === "-"){
-        return num1-num2
-    }
-    else if( operator==="*"){
-        return num1*num2
-    }
-    else if (operator=== "/"){
-        return num1/num2
-    }
-    else{
-        return "wrong entry"
-    }
-
+  if (operator === "+") {
+    return num1 + num2;
+  } else if (operator === "-") {
+    return num1 - num2;
+  } else if (operator === "*") {
+    return num1 * num2;
+  } else if (operator === "/") {
+    return num1 / num2;
+  } else {
+    return "wrong entry";
+  }
 }
-alert(calc())
+alert(calc());
 
 //square of argument
 
-function square(x){
-    return x*x
+function square(x) {
+  return x * x;
 }
 
-document.write("Square of Argument 20 ===> "+square(20))
+document.write("Square of Argument 20 ===> " + square(20));
 
 //factorial of a number
-document.write("<br><br><br>")
+document.write("<br><br><br>");
 
 function factorial(n) {
-    if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 
-document.write("factorial of 5 is === > "+factorial(5))
+document.write("factorial of 5 is === > " + factorial(5));
 
-document.write("<br><br><br>")
+document.write("<br><br><br>");
 
-//user input for starting and ending count 
-function numberCount(startNumber, endNumber){
-        for (i = startNumber; i<=endNumber;i++){
-            document.write ( i + "<br>")
-        }
-        
+//user input for starting and ending count
+function numberCount(startNumber, endNumber) {
+  for (i = startNumber; i <= endNumber; i++) {
+    document.write(i + "<br>");
+  }
 }
 
-    var start = +prompt("Write a Number to Start Counting:");
-    var end = +prompt("Write a Number to End Counting:");
+var start = +prompt("Write a Number to Start Counting:");
+var end = +prompt("Write a Number to End Counting:");
 
-    numberCount(start, end);
+numberCount(start, end);
 
-    
-    document.write("<br><br><br>")
+document.write("<br><br><br>");
 
-
-    //Hypontunuse calculation
+//Hypontunuse calculation
 function calculateHypotenuse(base, perpendicular) {
-    // Inner function to calculate the square of a number
-    function calculateSquare(side) {
-        return side * side;
-    }
+  // Inner function to calculate the square of a number
+  function calculateSquare(side) {
+    return side * side;
+  }
 
-    // Calculate the squares of base and perpendicular
-    var baseSquare = calculateSquare(base);
-    var perpendicularSquare = calculateSquare(perpendicular);
+  // Calculate the squares of base and perpendicular
+  var baseSquare = calculateSquare(base);
+  var perpendicularSquare = calculateSquare(perpendicular);
 
-    // Calculate the hypotenuse using the Pythagorean theorem
-    var hypotenuse = Math.sqrt(baseSquare + perpendicularSquare);
+  // Calculate the hypotenuse using the Pythagorean theorem
+  var hypotenuse = Math.sqrt(baseSquare + perpendicularSquare);
 
-    return hypotenuse;
+  return hypotenuse;
 }
 
 // Example usage
 var baseLength = parseFloat(prompt("Enter the length of the base:"));
-var perpendicularLength = parseFloat(prompt("Enter the length of the perpendicular:"));
+var perpendicularLength = parseFloat(
+  prompt("Enter the length of the perpendicular:")
+);
 
 var result = calculateHypotenuse(baseLength, perpendicularLength);
 document.write("Hypotenuse:", result);
 
 
+document.write("<br><br><br>");
 
 
+//Title Case 
+function titleCase(inputString) {
+  var words = inputString.split(" ");
+  var capitalizedWords = [];
+  for (i = 0; i < words.length; i++) {
+    var singleWord = words[i];
+    var capitalizedWord =
+      singleWord.charAt(0).toUpperCase() + singleWord.slice(1);
+    capitalizedWords.push(capitalizedWord);
+  }
+  return capitalizedWords.join(" ");
+}
 
+var inputString = prompt("Write any Sentence more than 2 Words:");
+var result = titleCase(inputString);
 
-
+document.write(result);
